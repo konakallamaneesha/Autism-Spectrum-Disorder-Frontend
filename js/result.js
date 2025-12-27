8c8ec3e0919399a9f61f1c2d0465128f0c556e5a
konakallamaneesha <konakallamaneesha13@gmail.com>
2025-12-27 10:56:36 +0530

    changes made

--- js/result.js
+++ js/result.js
@@ -2,15 +2,20 @@
 const result = JSON.parse(localStorage.getItem("asdResult"));
 
 if (result) {
+
     document.getElementById("prediction").innerText =
         result.prediction;
 
     document.getElementById("probability").innerText =
         "ASD Probability: " + result.probability;
 
+    // ⭐ ADD THIS ⭐
+    document.getElementById("severity").innerText =
+        "Severity Level: " + result.severity;
+
     const list = document.getElementById("explanation");
 
-    if (result.key_factors.length === 0) {
+    if (!result.key_factors || result.key_factors.length === 0) {
         list.innerHTML = "<li>No significant indicators</li>";
     } else {
         result.key_factors.forEach(item => {
